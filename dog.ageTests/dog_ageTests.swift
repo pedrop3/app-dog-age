64988095219fb239b9312ab3cc8e6270bfe4464b
Pedro Santos <pedro_henriquems321@hotmail.com>
2025-07-28 22:19:12 +0100

    Initial Commit

---
+++ dog.ageTests/dog_ageTests.swift
@@ -0,0 +1,17 @@
+//
+//  dog_ageTests.swift
+//  dog.ageTests
+//
+//  Created by Pedro Henrique Martins dos Santos on 7/28/25.
+//
+
+import Testing
+@testable import dog_age
+
+struct dog_ageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ dog.ageUITests/dog_ageUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  dog_ageUITestsLaunchTests.swift
+//  dog.ageUITests
+//
+//  Created by Pedro Henrique Martins dos Santos on 7/28/25.
+//
+
+import XCTest
+
+final class dog_ageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
